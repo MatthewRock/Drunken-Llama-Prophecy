16fff7bbbabbf5e67deac19310cbf84b3c404bec
Antystenes <morgoth21@gmail.com>
2015-04-12 01:23:57 +0200

    Added credits(Writing random and useless code)

---
+++ src/Credits.cpp
@@ -0,0 +1,59 @@
+#include "Credits.hpp"
+#include "MenuState.hpp"
+#include "GameEngine.hpp"
+namespace Llama
+{
+    CreditsState::CreditsState(GameEngine* eng) : m_win(eng->GetWindowPointer())
+    {
+        m_engine = eng;
+        m_credits.Init("media/Menu/Credits.png", *m_win);
+
+        m_musicManager.Insert(0, new Sounds::BGM("media/music/Sad sad mlg air horn.ogg"));
+        m_musicManager.Insert(1, new Sounds::BGM("media/music/SKRILLEX Scary.ogg"));
+        m_musicManager.Insert(2, new Sounds::BGM("media/music/2SAD4ME.ogg"));
+        m_musicIterator = m_musicManager.Beginning();
+
+        m_musicIterator->second->Play(0);
+    }
+
+    void CreditsState::Pause()
+    {
+        GameState::Pause();
+    }
+    void CreditsState::Resume()
+    {
+        GameState::Resume();
+    }
+    void CreditsState::Update()
+    {
+
+    }
+    void CreditsState::HandleEvents(SDL_Event& event)
+    {
+        if (!Sounds::BGM::Playing())
+        {
+            ++m_musicIterator;
+            if(m_musicIterator == m_musicManager.End())
+            {
+                m_musicIterator = m_musicManager.Beginning();
+            }
+            m_musicIterator->second->Play(0);
+        }
+        if (event.type == SDL_KEYDOWN)
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                ChangeStateDestructively(new MenuState(m_engine));
+            else
+            {
+                ++m_musicIterator;
+                if(m_musicIterator == m_musicManager.End())
+                {
+                    m_musicIterator = m_musicManager.Beginning();
+                }
+                m_musicIterator->second->Play(0);
+            }
+    }
+    void CreditsState::Draw()
+    {
+        m_credits.Draw(0,0);
+    }
+}
